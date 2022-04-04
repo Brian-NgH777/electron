daa01d710b6e7d57eadca93ac781e94ad5e38441
Brian nguyen <brian.ng142957@gmail.com>
2022-04-04 21:28:28 +0700

    Edit

--- mac.js
+++ mac.js
@@ -1,10 +1,14 @@
 // let {PythonShell} = require('python-shell');
 var path = require("path");
-const child_process = require("child_process");
-const fs = require("fs");
-let appName = "Electron", localData, fileName = "data-storage.log";
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+// const Redis = require('ioredis');
+// const fs = require("fs");
+// const client = new Redis('redis://localhost:6379');
 
-function GetIP() {
+// let appName = "Electron", localData, fileName = "data-storage.log";
+
+async function GetIP() {
     console.log("scan..........");
     document.getElementById("ip").innerHTML = "scan..........";
     var cmd = ""
@@ -29,103 +33,85 @@ function GetIP() {
         }
     }
 
-    run_script(cmd, null, () => {
-        console.log("running script success");
-    });
-
-    // PythonShell.run('scan.py', options, async function (err, data) {
-    //     if (err) throw err;
-    //     console.log('finished');
-    //     let strArr = data[0].replace(/'/g, '"');
-    //     strArr = JSON.parse(strArr);
-    //     let df = [];
-    //     for(let i = 0 ; i < strArr.length; i ++) {
-    //         try {
-    //             const response = await axios.get(`https://api.maclookup.app/v2/macs/${strArr[i].mac}`);
-    //             d = strArr[i];
-    //             d.verdor = response.data;
-    //             df.push(d);
-    //         } catch (error) {
-    //             throw error;
-    //         }
-    //     }
-
-    //     document.getElementById("ip").innerHTML = df;
-    // });
+    const { stdout, stderr } = await exec(cmd);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+    // await saveDataScan('b-test-1', stdout);
+    document.getElementById("ip").innerHTML = stdout;
 }
 
-function run_script(command, args, callback) {
-    var child = child_process.spawn(command, args, {
-      encoding: "utf8",
-      shell: true,
-    });
-    // You can also use a variable to save the output for when the script closes later
-    child.on("error", (error) => {
-      console.warn("Error occured");
-    });
-  
-    child.stdout.setEncoding("utf8");
-    child.stdout.on("data", (data) => {
-      //Here is the output
-        console.log("data", data);
-        // saveAppData(data)
-        document.getElementById("ip").innerHTML = data;
-    });
-  
-    child.stderr.setEncoding("utf8");
-    child.stderr.on("data", (data) => {
-      // Return some data to the renderer process with the mainprocess-response ID
-    //   mainWindow.webContents.send("mainprocess-response", data);
-      //Here is the output from the command
-      console.log("error", data);
-    });
-  
-    child.on("close", (code) => {
-      //Here you can get the exit code of the script
-      switch (code) {
-        case 0:
-          console.info("End process.");
-          break;
-      }
-    });
-    if (typeof callback === "function") callback();
-}
 
-function getAppDataPath() {
+// function getAppDataPath() {
+//     switch (process.platform) {
+//         case "darwin": {
+//             return path.join(process.env.HOME, "Library", "Application Support", appName);
+//         }
+//         case "win32": {
+//             return path.join(process.env.APPDATA, appName);
+//         }
+//         case "linux": {
+//             return path.join(process.env.HOME, "."+appName);
+//         }
+//         default: {
+//             console.log("Unsupported platform!");
+//             process.exit(1);
+//         }
+//     }
+// }
+
+// function saveAppData (content) {
+//     const appDataDir = getAppDataPath();
+
+//     // Create appDataDir if not exist
+//     if (!fs.existsSync(appDataDir)) {
+//         fs.mkdirSync(appDataDir);
+//     }
+
+//     const appDataFilePath = path.join(appDataDir, fileName);
+//     content = JSON.stringify(content);
+
+//     fs.writeFile(appDataFilePath, content, (err) => {
+//         if (err) {
+//             console.log("There was a problem saving data!");
+//             // console.log(err);
+//         } else {
+//             console.log("Data saved correctly!");
+//         }
+//     });
+// }
+
+// async function saveDataScan(key, value) {
+//     let s = await client.set(key, value);
+//     console.log("set", s);
+// }
+
+async function InstallPackage() {
+    let pab = '';
+    let pai = path.join(__dirname, 'frp', 'frpc.ini');
     switch (process.platform) {
         case "darwin": {
-            return path.join(process.env.HOME, "Library", "Application Support", appName);
+            console.log("darwin");
+            pab = path.join(__dirname, 'frp', 'frpc-mac');
+            break
         }
         case "win32": {
-            return path.join(process.env.APPDATA, appName);
+            console.log("win32");
+            pab = path.join(__dirname, 'frp', 'frpc-win.exe');
+            break
         }
         case "linux": {
-            return path.join(process.env.HOME, "."+appName);
+            console.log("linux");
+            pab = path.join(__dirname, 'frp', 'frpc-raspberry');
+            break
         }
         default: {
-            console.log("Unsupported platform!");
-            process.exit(1);
+            pab = path.join(__dirname, 'frp', 'frpc-mac');
         }
     }
+
+    let cmd = `${pab} -c ${pai}`
+    console.log('cmd:', cmd);
+    const { stdout, stderr } = await exec(cmd);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
 }
-
-function saveAppData (content) {
-    const appDataDir = getAppDataPath();
-
-    // Create appDataDir if not exist
-    if (!fs.existsSync(appDataDir)) {
-        fs.mkdirSync(appDataDir);
-    }
-
-    const appDataFilePath = path.join(appDataDir, fileName);
-    content = JSON.stringify(content);
-
-    fs.writeFile(appDataFilePath, content, (err) => {
-        if (err) {
-            console.log("There was a problem saving data!");
-            // console.log(err);
-        } else {
-            console.log("Data saved correctly!");
-        }
-    });
-} 
