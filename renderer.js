e7deae559595eab9859438591f0d28a75aeee95d
nguyenthanh1205tb <nguyenthanh1205tb@gmail.com>
2022-04-05 14:04:18 +0700

    basic ui with vue

--- renderer.js
+++ renderer.js
@@ -1,6 +1,22 @@
+const SetUp = require('./app/pages/SetUp');
+const Login = require('./app/pages/Auth/Login');
 
+const routes = [
+  { path: '/logged', component: SetUp },
+  { path: '/', component: Login },
+];
 
-function Ping() {
-    console.log("pong pong pong");
-    document.getElementById("demo").innerHTML = "Hello World";
-}
+// Create App
+const App = Vue.createApp({});
+
+// Router
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+});
+
+// App use
+App.use(router);
+
+// Mount App
+App.mount('#app');
