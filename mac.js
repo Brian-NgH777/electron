e9525723f64db6b20e184d9b3ed342f9e57fa40c
Brian nguyen <brian.ng142957@gmail.com>
2022-04-05 17:06:59 +0700

    Reslove conflict

--- mac.js
+++ mac.js
@@ -53,20 +53,6 @@ async function GetIP(username) {
             cmd = path.join(__dirname, 'py', 'pk-new');
         }
     }
-    case 'win32': {
-      console.log('win32');
-      cmd = path.join(__dirname, '../', 'py', 'pk-new.exe');
-      break;
-    }
-    case 'linux': {
-      console.log('linux');
-      cmd = path.join(__dirname, '../', 'py', 'pk-new');
-      break;
-    }
-    default: {
-      cmd = path.join(__dirname, '../', 'py', 'pk-new');
-    }
-  }
 
     const { stdout, stderr } = await exec(cmd);
     console.log('stdout:', stdout);
@@ -151,26 +137,12 @@ async function InstallPackage() {
             pab = path.join(__dirname, 'frp', 'frpc-mac');
         }
     }
-    case 'win32': {
-      console.log('win32');
-      pab = path.join(__dirname, '../', 'frp', 'frpc-win.exe');
-      break;
-    }
-    case 'linux': {
-      console.log('linux');
-      pab = path.join(__dirname, '../', 'frp', 'frpc-raspberry');
-      break;
-    }
-    default: {
-      pab = path.join(__dirname, '../', 'frp', 'frpc-mac');
-    }
-  }
-
-  let cmd = `${pab} -c ${pai}`;
-  console.log('cmd:', cmd);
-  const { stdout, stderr } = await exec(cmd);
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
+  
+    let cmd = `${pab} -c ${pai}`;
+    console.log('cmd:', cmd);
+    const { stdout, stderr } = await exec(cmd);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
 }
 
 function createIni(d) {
